a7afb1b17f1d5ec714ea5858abaacac09ebfdf5c
verevinds <verevinds@yandex.ru>
2020-04-28 13:31:58 +0700

    Add method Delete in the ListEdit

--- src/component/ListEdit/ListEdit.jsx
+++ src/component/ListEdit/ListEdit.jsx
@@ -2,8 +2,11 @@ import React, { memo, useState, useEffect } from 'react';
 import styles from './styles.module.css';
 
 //? Bootstrap
-import { Col, Container, ListGroup, Form } from 'react-bootstrap';
+import { Row, Col, Container, ListGroup, Form, Button } from 'react-bootstrap';
 import { useDispatch } from 'react-redux';
+//? Font Awesome
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
 const ListEdit = (props) => {
   const dispatch = useDispatch();
@@ -21,8 +24,11 @@ const ListEdit = (props) => {
   // Define function handle submit
   const onSubmit = (event) => {
     event.preventDefault();
-    dispatch(props.actionCreator(state));
-    setState({ name: '' });
+    if (props.route) {
+      console.log('route', props.route);
+      dispatch(props.actionCreator(props.route, 'post', state));
+      setState({ name: '' });
+    }
   };
   return (
     <Col xs={3}>
@@ -47,18 +53,40 @@ const ListEdit = (props) => {
             props.list.map((item) => (
               <ListGroup.Item
                 key={item.id}
-                className={
-                  item.id === props.activeId
-                    ? `${styles.item} active`
-                    : styles.item
-                }
-                onClick={() => {
-                  if (props.onClick) {
-                    props.onClick(item.id, props.setNumber || null);
-                  }
-                }}
+                className={item.id === props.activeId ? `active` : null}
               >
-                {item.name}
+                <Row>
+                  <Col xs={9}>
+                    <div
+                      className={styles.item}
+                      onClick={() => {
+                        if (props.onClick) {
+                          props.onClick(item.id, props.setNumber || null);
+                        }
+                      }}
+                    >
+                      {item.name}
+                    </div>
+                  </Col>
+                  <Col xs={3}>
+                    <Button
+                      size="sm"
+                      variant="danger"
+                      onClick={() => {
+                        dispatch(
+                          props.actionCreator(
+                            props.route,
+                            'delete',
+                            '',
+                            item.id,
+                          ),
+                        );
+                      }}
+                    >
+                      <FontAwesomeIcon icon={faTrash} />
+                    </Button>
+                  </Col>
+                </Row>
               </ListGroup.Item>
             ))
           ) : (
