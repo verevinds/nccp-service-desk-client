bc26f2323e7b5194d07543b771df08dd8fc2598d
verevinds <verevinds@yandex.ru>
2020-04-28 11:38:42 +0700

    Create "pure" component ListEdit

--- src/component/ListEdit/ListEdit.jsx
+++ src/component/ListEdit/ListEdit.jsx
@@ -1,26 +1,70 @@
-import React, { memo } from 'react';
+import React, { memo, useState, useEffect } from 'react';
+import styles from './styles.module.css';
 
-//?Bootstrap
-import { Col, Container } from 'react-bootstrap';
+//? Bootstrap
+import { Col, Container, ListGroup, Form } from 'react-bootstrap';
+import { useDispatch } from 'react-redux';
 
 const ListEdit = (props) => {
-  console.log(props);
+  const dispatch = useDispatch();
+
+  //! Определить локальное состояние и обработчик
+  // Define local state and handleState
+  const [state, setState] = useState({ name: '' });
+  useEffect(() => {
+    if (props.categoryId) {
+      setState({ name: '', categoryId: props.categoryId });
+    }
+  }, [props.categoryId]);
+
+  //! Определить функцию обработки события
+  // Define function handle submit
+  const onSubmit = (event) => {
+    event.preventDefault();
+    dispatch(props.actionCreator(state));
+    setState({ name: '' });
+  };
   return (
-    <Col>
+    <Col xs={3}>
       <Container>
-        <h2></h2>
-        {props.list
-          ? props.list.map((item) => (
-              <li
+        <Form onSubmit={onSubmit}>
+          <Form.Group>
+            <Form.Label>
+              <h2>{props.title}</h2>
+            </Form.Label>
+            <Form.Control
+              type="text"
+              placeholder='Введите название и нажмите "Enter"'
+              value={state.name}
+              onChange={(event) => {
+                setState({ ...state, name: event.target.value });
+              }}
+            />
+          </Form.Group>
+        </Form>
+        <ListGroup variant="flush">
+          {props.list ? (
+            props.list.map((item) => (
+              <ListGroup.Item
                 key={item.id}
+                className={
+                  item.id === props.activeId
+                    ? `${styles.item} active`
+                    : styles.item
+                }
                 onClick={() => {
-                  props.onClick(item.id);
+                  if (props.onClick) {
+                    props.onClick(item.id, props.setNumber || null);
+                  }
                 }}
               >
                 {item.name}
-              </li>
+              </ListGroup.Item>
             ))
-          : null}
+          ) : (
+            <small className="text-muted text-center">Данные отсутствуют</small>
+          )}
+        </ListGroup>
       </Container>
     </Col>
   );
