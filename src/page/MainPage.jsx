089e2675898ea373069cf8d56d52f67e8993600e
Веревин Денис Сергеевич <vds81251@c31.nccp.ru>
2020-04-29 14:26:41 +0700

    Select incident from the state

--- src/page/MainPage.jsx
+++ src/page/MainPage.jsx
@@ -1,15 +1,21 @@
-import React, { memo } from 'react';
+import React, { memo, useEffect } from 'react';
 
 /**Bootstrap components */
 import { Row, Col } from 'react-bootstrap';
 import Incident from '../component/Incident/Incident';
 import Sidebar from '../component/Sidebar/Sidebar';
+import { useSelector } from 'react-redux';
 
 const MainPage = () => {
+  const incidents = useSelector((state) => state.incidents);
+  useEffect(() => {
+    console.log(incidents);
+  }, [incidents]);
+
   return (
     <Row className="mt-3">
       <Col xs={3}>
-        <Sidebar />
+        <Sidebar list={incidents.list} isLoading={incidents.isLoading} />
       </Col>
       <Col>
         <Incident />
