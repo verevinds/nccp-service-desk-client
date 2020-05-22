6535acdbf2327521c454c7cc7d30850737800700
verevinds <verevinds@yandex.ru>
2020-05-22 09:54:57 +0700

    Proxy edit

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ const proxy = require('http-proxy-middleware');
 module.exports = function (app) {
   app.use(
     proxy('/api', {
-      target: 'http://localhost',
+      target: 'http://192.168.213.77',
       secure: false,
       changeOrigin: true,
     }),
