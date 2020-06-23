ed5183e25ec0f233bb9dd29a5a7c71ad00051ab8
verevinds <verevinds@yandex.ru>
2020-06-23 11:35:32 +0700

    Fix info

--- src/page/InfoPage.jsx
+++ src/page/InfoPage.jsx
@@ -4,7 +4,35 @@ import { Container, Card, Accordion, Button } from 'react-bootstrap';
 const InfoPage = () => {
   const info = [
     {
-      version: `1.1.0`,
+      version: `1.3.1`,
+      date: `23.06.2020`,
+      body: [
+        {
+          title: `Приложение > Вертикальное меню`,
+          text:
+            'В вертикальном меню приложения, где находится список заявок, добавлен фильтр. Можно выбрать сортировку: номеру; имени; сроку сдачи.',
+        },
+      ],
+    },
+    {
+      version: `1.3.0`,
+      date: `22.06.2020`,
+      body: [
+        {
+          title: `Каталог > Параметры`,
+          text:
+            'Настройка уникальной формы заполнения для отдельных параметров. Если Вам не подходить форма заявки по умолчанию, в настройках можно изменить эту форму на свою.',
+        },
+
+        {
+          title: `Заявки > Перевод в другой отдел`,
+          text: 'Перевод в другой отдел, только после согласования руководителем.',
+        },
+      ],
+    },
+    {
+      version: `1.2.0`,
+      date: `18.06.2020`,
       body: [
         {
           title: `Заявки`,
@@ -18,21 +46,8 @@ const InfoPage = () => {
       ],
     },
     {
-      version: `1.0.3`,
-      body: [
-        {
-          title: `Заявки`,
-          text: 'Заявки можно переводить в другой',
-        },
-
-        {
-          title: `Заявки > Перевод в другой отдел`,
-          text: 'Перевод в другой отдел, только после согласования руководителем.',
-        },
-      ],
-    },
-    {
-      version: `1.0.2`,
+      version: `1.1.2`,
+      date: `16.06.2020`,
       body: [
         {
           title: `Настройки > Суперпользователь`,
@@ -46,7 +61,8 @@ const InfoPage = () => {
       ],
     },
     {
-      version: '1.0.1',
+      version: '1.1.1',
+      date: `15.06.2020`,
       body: [
         { title: `Приложение > Структура`, text: `Связь один к многому между таблицами параметр и опция` },
         {
@@ -57,15 +73,34 @@ const InfoPage = () => {
       ],
     },
   ];
+  let news = info[0];
+  info.splice(0, 1);
   return (
     <Container fluid>
-      <h1>Документация (версия {`${process.env.REACT_APP_VERSION}`})</h1>
+      <h1>Что нового </h1>
+      <br />
+      <hr />
+      <br />
+      <h6>Текущая версия: {`${news.version}`}</h6>
+      {news.body.map((item) => (
+        <>
+          <Card.Title>{item.title}</Card.Title>
+          <Card.Text>{item.text}</Card.Text>
+        </>
+      ))}
+      <br />
+      <hr />
+      <br />
+      <h6>История:</h6>
       <Accordion defaultActiveKey="0">
         {info.map((item, index) => (
           <Card>
             <Card.Header>
               <Accordion.Toggle as={Button} variant="link" eventKey={`${index}`}>
-                Версия {item.version}
+                Версия {item.version}{' '}
+                <small>
+                  (<i>{item.date}</i>)
+                </small>
               </Accordion.Toggle>
             </Card.Header>
             <Accordion.Collapse eventKey={`${index}`}>
